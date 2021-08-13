5415a5a1c156b571c3a295dee716515f3f27e638
ranga <rangainlive@gmail.com>
2021-08-13 08:02:33 +0530

    redux-store, actions & reducers created for posts

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
 
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers/index";
+import { applyMiddleware, createStore } from "redux";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
